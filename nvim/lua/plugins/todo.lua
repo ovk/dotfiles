b0ba8b2c13ead658935a7287a66b3877d29a0588
ovk <mail@okosh.xyz>
2024-07-16 22:05:09 -0400

    Neovim: add Folke TODO comments plugin

---
+++ nvim/lua/plugins/todo.lua
@@ -0,0 +1,15 @@
+return {
+    'folke/todo-comments.nvim',
+
+    lazy = false,
+
+    dependencies = { 'nvim-lua/plenary.nvim' },
+
+    opts = {
+        keywords = {
+            TODO = { icon = "", color = "info" },
+            PERF = { icon = "⏲", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+            TEST = { icon = "󰙨", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+        }
+    }
+}
