f2548e8fa77bc83ef454c39e6e0c1cb25e32e9c6
ovk <mail@okosh.xyz>
2024-08-24 20:28:01 -0400

    Neovim: add markview.nvim plugin

---
+++ nvim/lua/plugins/markview.lua
@@ -0,0 +1,10 @@
+return {
+    'OXY2DEV/markview.nvim',
+    lazy = true,
+    ft = 'markdown',
+
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter',
+        'nvim-tree/nvim-web-devicons'
+    }
+}
